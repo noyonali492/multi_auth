e821d7f235466e99c9a3768d69e398a54a499c20
noyonali492 <noyonahmed492@gmail.com>
2024-09-18 22:03:03 +0600

    Auth_multi

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([ 
+                [
+                    'name' => 'Admin',
+                    'email' => 'admin@gmail.com',
+                    'password' => bcrypt('12345678'),
+                    'role' => 'admin'
+                ],
+
+                [
+                    'name' => 'Vendor',
+                    'email' => 'vendor@gmail.com',
+                    'password' => bcrypt('12345678'),
+                    'role' => 'vendor'
+                ],
+
+                [
+                    'name' => 'Customer',
+                    'email' => 'customer@gmail.com',
+                    'password' => bcrypt('12345678'),
+                    'role' => 'customer'
+                ]
+            ]);
+    }
+}
